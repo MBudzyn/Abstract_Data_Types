7872855d09a9d9ee049ee501e292b1b8c56e1179
Mateusz Budzyński <budzynekm09@gmail.com>
2023-07-20 10:44:21 +0200

    Node and CSLL class

---
+++ CircularSinglyLinkedList.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+
+class CircularSinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
