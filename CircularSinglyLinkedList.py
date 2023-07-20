b1c92850cf9c39d986e0a06ddb5d93b90dc136c0
Mateusz Budzyński <budzynekm09@gmail.com>
2023-07-20 10:46:59 +0200

    iter method

--- CircularSinglyLinkedList.py
+++ CircularSinglyLinkedList.py
@@ -9,3 +9,10 @@ class CircularSinglyLinkedList:
         self.head = None
         self.tail = None
 
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            if node.next == self.head:
+                break
+            node = node.next
