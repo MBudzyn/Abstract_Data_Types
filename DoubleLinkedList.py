0dfb1593b01b1b950ced7d0096a27662c0782593
Mateusz Budzyński <budzynekm09@gmail.com>
2023-07-22 09:49:04 +0200

    Node and DLL class

---
+++ DoubleLinkedList.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self,value=None):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
+
