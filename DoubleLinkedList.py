0ec4478717dbda822d9aa871d51aeb6a9f7c9754
Mateusz Budzyński <budzynekm09@gmail.com>
2023-07-22 09:51:41 +0200

    __iter__ method

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -8,6 +8,11 @@ class DoubleLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+    def __iter__(self):
+        iter_node = self.head
+        while iter_node:
+            yield iter_node
+            iter_node = iter_node.next
 
 
 
